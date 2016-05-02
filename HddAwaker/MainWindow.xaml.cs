09e976400229c73ab0a550358d36d73d94a82738
Jackson Ming Hu <huming2207@gmail.com>
2016-05-02 20:47:15 +1000

    Now it can hide into the tray

--- HddAwaker/MainWindow.xaml.cs
+++ HddAwaker/MainWindow.xaml.cs
@@ -14,8 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Windows.Forms;
 using Ini.Net;
-using Ini;
 
 namespace HddAwaker
 {
@@ -27,11 +27,32 @@ namespace HddAwaker
         public MainWindow()
         {
             InitializeComponent();
+
+            // See this for more detail: https://social.msdn.microsoft.com/Forums/vstudio/en-US/21992d0b-a02c-4042-a188-47b0a2b99b0b/wpf-system-tray-application?forum=wpf
+            // Icon comes from EasyIcon. Thanks mate!
+
+            NotifyIcon notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = Properties.Resources.hddlogo;
+            notifyIcon.Visible = true;
+            notifyIcon.DoubleClick +=
+                delegate (object sender, EventArgs args)
+                {
+                    this.Show();
+                    this.WindowState = WindowState.Normal;
+                };
         }
 
         private static string driveLetterSettings = string.Empty;
         private static int durationTimeSettings = 30;
         private DispatcherTimer timer = new DispatcherTimer();
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                this.Hide();
+
+            base.OnStateChanged(e);
+        }
 
 
         private void button_Click(object sender, RoutedEventArgs e)
@@ -45,6 +66,7 @@ namespace HddAwaker
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            this.Hide();
             refreshDeviceList();
         }
 
@@ -81,7 +103,7 @@ namespace HddAwaker
             configFile.WriteString("Settings", "Drive", driveLetter);
             configFile.WriteInteger("Settings", "Duration", durationSecond);
             stopTimer(); startTimer(durationSecond);
-            MessageBox.Show("Setting saved!\r\n" + "Value: " + durationSecond + " seconds.");
+            System.Windows.MessageBox.Show("Setting saved!\r\n" + "Value: " + durationSecond + " seconds.");
         }
 
         private void readSettings()
@@ -97,12 +119,12 @@ namespace HddAwaker
             Random randomGenerator = new Random();
             randomGenerator.NextBytes(writtenArray);
 
-            Directory.CreateDirectory(driveLetterSettings + @"awaker");
-            
+            DirectoryInfo directoryInfo = Directory.CreateDirectory(driveLetterSettings + @"awaker");
+            directoryInfo.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+
             FileStream fileStream = File.Create(driveLetterSettings + @"\awaker\awaker.bin");
             await fileStream.WriteAsync(writtenArray, 0, 1024);
             fileStream.Dispose();
-            
         }
 
     }
